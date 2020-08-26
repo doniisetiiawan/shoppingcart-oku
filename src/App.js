ff8db318908fdb7bba1a6c3878786d36da52921c
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-26 10:19:54 +0700

    Stateful Functional Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './header';
 import ProductList from './productList';
 import ShoppingCart from './shoppingCart';
 import './App.css';
 
+function getProductQuantity(map, product) {
+  const existingProduct = map[product.id];
+  return existingProduct ? existingProduct.quantity : 0;
+}
+
+function addProductToMap(map, product) {
+  const newMap = { ...map };
+  const quantity = getProductQuantity(map, product) + 1;
+  newMap[product.id] = { ...product, quantity };
+  return Object.freeze(newMap);
+}
+
+function removeProductFromMap(map, product) {
+  const newMap = { ...map };
+  delete newMap[product.id];
+  return Object.freeze(newMap);
+}
+
+function addPrice(totalPrice, line) {
+  return totalPrice + line.price * line.quantity;
+}
+
+function toCartView(map) {
+  const list = Object.values(map);
+  return Object.freeze({
+    list,
+    total: list.reduce(addPrice, 0),
+  });
+}
+
 function App({ products }) {
-  const cart = { list: products };
+  const [shoppingMap, setShoppingMap] = useState({});
 
   function addToCart(product) {
-    console.log(`add ${product.id}`);
+    setShoppingMap((map) => addProductToMap(map, product));
   }
 
   function removeFromCart(product) {
-    console.log(`remove ${product.id}`);
+    setShoppingMap((map) => removeProductFromMap(map, product));
   }
 
   return (
@@ -25,11 +55,12 @@ function App({ products }) {
         />
 
         <ShoppingCart
-          cart={cart}
+          cart={toCartView(shoppingMap)}
           onRemoveClick={removeFromCart}
         />
       </div>
     </div>
   );
 }
+
 export default App;
--- src/header.js
+++ src/header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <h1>A Shopping Cart</h1>
+    </header>
   );
 }
 
