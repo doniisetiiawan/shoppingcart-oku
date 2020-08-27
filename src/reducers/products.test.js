2fed81ba65adae83a114c0fb88915e5167beaa20
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-27 11:24:43 +0700

    Testing

---
+++ src/reducers/products.test.js
@@ -0,0 +1,23 @@
+import actions from '../actions/productsActions';
+import productsReducer from './products';
+
+test('products() can reset all products', () => {
+  // arrange
+  const products = [];
+  const newProducts = [
+    { id: 1, name: 'apple', price: 10 },
+    { id: 2, name: 'mango', price: 5 },
+  ];
+
+  // act
+  const resetProductsAction = actions.resetProducts(
+    newProducts,
+  );
+  const result = productsReducer(
+    products,
+    resetProductsAction,
+  );
+
+  // assert
+  expect(Array.from(result)).toEqual(newProducts);
+});
---
+++ src/selectors/shoppingCartSelectors.test.js
@@ -0,0 +1,26 @@
+import { Map } from 'immutable';
+import selectors from './ShoppingCartSelectors';
+
+test('toCartView() can compute total price', () => {
+  // arrange
+  const shoppingCart = Map({
+    1: {
+      id: 1,
+      title: 'apple',
+      price: 10,
+      quantity: 1,
+    },
+    2: {
+      id: 2,
+      title: 'mango',
+      price: 5,
+      quantity: 1,
+    },
+  });
+
+  // act
+  const cart = selectors.toCartView({ shoppingCart });
+
+  // assert
+  expect(cart.total).toEqual(15);
+});
